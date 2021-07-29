36db451bf46f7bc00893cead0090b5010f913539
Benjamin Fultz <xsymbiotex@gmail.com>
2021-07-29 15:38:25 -0400

    moar mvc

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const connectDB = require('./config/database');
 
 const app = express();
-const PORT = process.env.PORT || 9000;
-dotenv.config();
+
+require('dotenv').config({path: './config/.env'})
+
+connectDB();
 
 app.use(cors());
 
@@ -13,6 +15,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirName + '/index.html')
 });
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running.`)
+})
